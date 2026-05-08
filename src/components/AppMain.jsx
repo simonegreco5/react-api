80ee89dea29079084449e7ce013c16c23d9d7341
simonegreco5 <simoxey.greco@gmail.com>
2026-05-08 23:26:46 +0200

    - add bootstrap and bootstrap-icons.  - uppdate AppMain component for rendering actors cards

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -7,10 +7,29 @@ export default function AppMain(){
     const api_attori = 'https://lanciweb.github.io/demo/api/actors/'
     const api_attrici = 'https://lanciweb.github.io/demo/api/actresses/'
 
-    const [attori, setAttori] = useState()
+    const [attori, setAttori] = useState(api_attori)
 
 
     return (
+        <main>
+            <div className="container">
+                    {
+                        attori.map((item) => (
+                            <>
+                            <div className="card" key={item.id}>
+                              <img src={item.image} class="card-img-top" alt="attore" />
+                              <div className="card-body">
+                                <h5 className="card-title">{item.name}</h5>
+                                <h6 class="card-subtitle mb-2 text-body-secondary">{item.birth_year}{item.nationality}</h6>
+                                <h6 class="card-subtitle mb-2 text-body-secondary">{item.awards}</h6>
+                                <p className="card-text">{item.biography}</p>
+                              </div>
+                            </div>
+                            </>
+                        ))
+                    }
+            </div>
+        </main>
 
     )
 }
