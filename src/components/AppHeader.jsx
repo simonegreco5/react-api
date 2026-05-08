df053d003ed3ead163877416f1c5eecbe16cd991
simonegreco5 <simoxey.greco@gmail.com>
2026-05-08 22:48:20 +0200

    first commit

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,9 @@
+export default function AppHeader (){
+
+    return (
+        <header>
+            <h1>Cast Actors</h1>
+            <h5>by BooleanFilm</h5>
+        </header>
+    )
+}
---
+++ src/components/AppMain.jsx
@@ -0,0 +1,16 @@
+// sezione import
+import { useState } from "react"
+
+export default function AppMain(){
+
+    // chiamate api
+    const api_attori = 'https://lanciweb.github.io/demo/api/actors/'
+    const api_attrici = 'https://lanciweb.github.io/demo/api/actresses/'
+
+    const [attori, setAttori] = useState()
+
+
+    return (
+
+    )
+}
